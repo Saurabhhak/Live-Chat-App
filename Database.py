f72f00a6c8d305a7b3b0fe0fe7d2f1ce2b6f348f
Saurabh <skkashyap2328@gmail.com>
2024-05-19 00:33:15 +0530

    YOUR OWN COMMENT

---
+++ Database.py
@@ -0,0 +1,12 @@
+# Database.py
+# import firebase_admin
+# from firebase_admin import credentials
+# from firebase_admin import db
+
+# cred = credentials.Certificate("serviceAccountKey.json")
+# firebase_admin.initialize_app(cred, {'databaseURL': ""})
+
+# ref = db.reference('Students')
+
+# for key, value in data.items():
+#   ref.child(key).set(value)
